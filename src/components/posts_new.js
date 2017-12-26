9f9fb9425911b818c5ba61b5ca625a61f14605d7
Matt Rakestraw <rake36@gmail.com>
2017-12-26 16:37:01 -0500

    up to generalized fields

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,11 +1,46 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
-export default class PostsNew extends Component {
-    render() {
+class PostsNew extends Component {
+
+    renderField(field) {
+        // ...field.input maps all the properties to the <input> tag
+        //   without having to do it manually one by one
         return (
-            <div>
-                PostsNew!
+            <div className="form-group">
+                <label>{field.label}</label>
+                <input className="form-control"
+                    type="text"
+                    {...field.input}
+                />
             </div>
         );
     }
+
+    render() {
+        // any arbitrary property assigned to Field is available on the field in the renderer, like label...
+        return (
+            <form>
+                <Field
+                    label="Title"
+                    name="title"
+                    component={this.renderField}
+                />
+                <Field
+                    label="Tags"
+                    name="tags"
+                    component={this.renderField}
+                />
+                <Field
+                    label="Post Content"
+                    name="content"
+                    component={this.renderField}
+                />
+            </form>
+        );
+    }
 }
+
+export default reduxForm({
+    form: 'PostsNewForm'
+})(PostsNew);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,12 @@
 import { combineReducers } from 'redux';
 import PostsReducer from './reducer_posts';
+import { reducer as formReducer } from 'redux-form';
+
+// very important that the formReducer is assigned to 'form'... coding by convention
 
 const rootReducer = combineReducers({
-  posts: PostsReducer
+  posts: PostsReducer,
+  form: formReducer  // !!!
 });
 
 export default rootReducer;
