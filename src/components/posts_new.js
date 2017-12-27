0a43cd9fa366e56de740dcbe268025702a039954
Matt Rakestraw <rake36@gmail.com>
2017-12-27 10:34:49 -0500

    validate form and cancel post

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,34 +1,51 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
 
 class PostsNew extends Component {
 
     renderField(field) {
         // ...field.input maps all the properties to the <input> tag
         //   without having to do it manually one by one
+
+        const { meta: { touched, error } } = field;  // destructuring: meta, touched, error all assigned to relevant properties in field
+
+        const className = `form-group ${touched && error ? 'has-danger' : ''}`;
+
         return (
-            <div className="form-group">
+            <div className={className}>
                 <label>{field.label}</label>
                 <input className="form-control"
                     type="text"
                     {...field.input}
                 />
+                <div className="text-help">
+                    {touched ? error : ''}
+                </div>
             </div>
         );
     }
 
+    onSubmit(values) {
+        // this === component due to .bind(this) in onSubmit wiring
+        console.log(values);
+    }
+
     render() {
+
+        const { handleSubmit } = this.props;
+
         // any arbitrary property assigned to Field is available on the field in the renderer, like label...
         return (
-            <form>
+            <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
                 <Field
                     label="Title"
                     name="title"
                     component={this.renderField}
                 />
                 <Field
-                    label="Tags"
-                    name="tags"
+                    label="Categories"
+                    name="categories"
                     component={this.renderField}
                 />
                 <Field
@@ -36,11 +53,32 @@ class PostsNew extends Component {
                     name="content"
                     component={this.renderField}
                 />
+                <button type="submit" className="btn btn-primary">Submit</button>
+                <Link to="/" className="btn btn-danger">Cancel</Link>
             </form>
         );
     }
 }
 
+function validate(values) {
+    // console.log(values) -> { title: 'asdf', categories: 'asdf', etc.}
+    const errors = {};
+
+    // validate the inputs from 'values'
+    if (!values.title || values.title.length < 3) {
+        errors.title = "Enter a title of at least 3 chars...";
+    }
+    if (!values.categories) {
+        errors.categories = "Enter at least one category...";
+    }
+    if (!values.content) {
+        errors.content = "Enter some content...";
+    }
+
+    return errors;  // if errors is empty, passes validation
+}
+
 export default reduxForm({
+    validate,
     form: 'PostsNewForm'
 })(PostsNew);
